bb86ffb919c6c750b0f7d66964ddfb2c4cfe7839
B-Phuong <55148435+B-Phuong@users.noreply.github.com>
2021-04-13 22:54:57 +0700

    LoadThongTinNV

--- Quanlybaidoxe/Quanlybaidoxe/BS Layer/BLNhanVien.cs
+++ Quanlybaidoxe/Quanlybaidoxe/BS Layer/BLNhanVien.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using Quanlybaidoxe.DB_Layer;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace Quanlybaidoxe.BS_Layer
 {
     class BLNhanVien
     {
+        public DBQLBaiDoXe db = null;
+        public BLNhanVien()
+        {
+            db = new DBQLBaiDoXe();
+        }
+
+        public DataSet LayNV()
+        {
+            return db.ExecuteQueryDataSet("SELECT TaiKhoan.MaNV, TenNV, NgaySinh, GioiTinh, CMND, SDT, DiaChi, TaiKhoan, MatKhau FROM TaiKhoan, NhanVien WHERE TaiKhoan.MaNV = NhanVien.MaNV AND NhanVien.MaCV = 'CV02'", CommandType.Text);
+        }
+
     }
 }
